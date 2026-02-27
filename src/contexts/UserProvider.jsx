e4f93e70af0a0180e5fe1d0b11b2f5a42da29941
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-02-27 13:51:25 +0000

    added UserProvider context

---
+++ src/contexts/UserProvider.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = (props) => {
+  const [loggedInUser, setLoggedInUser] = useState({
+    userName: "No User Selected",
+    avatar_url: "../../public/images.png",
+  });
+
+  return (
+    <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
