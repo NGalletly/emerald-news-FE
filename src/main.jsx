0351fcb519ae5f6621dc6f2627edad42451e6248
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-02-27 13:55:50 +0000

    added context

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import { UserProvider } from "./contexts/UserContext";
+import { UserProvider } from "./contexts/UserProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
