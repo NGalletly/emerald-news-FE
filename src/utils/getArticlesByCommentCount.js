fae3b140776d31636a6462acded8d33098c8ea73
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-03-11 21:44:30 +0000

    edited util function

--- src/utils/getArticlesByCommentCount.js
+++ src/utils/getArticlesByCommentCount.js
@@ -1,6 +1,6 @@
 export function getArticlesByCommentCount() {
   return fetch(
-    `https://nevilles-news.onrender.com/api/articles?sort_by=comment_count&order_by=desc`,
+    `https://https://emerald-news.onrender.com/api/articles?sort_by=comment_count&order_by=desc`,
   ).then((res) => {
     if (!res.ok) {
       return Promise.reject({
